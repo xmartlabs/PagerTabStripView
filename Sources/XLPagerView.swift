91c00a6d8533deada2a9ad85ecb9a6d88d837b23
Manuel Lorenze <manuel@xmartlabs.com>
2020-07-28 22:12:18 -0300

    add currentOffset property and rename currentPage property

--- Sources/XLPagerView.swift
+++ Sources/XLPagerView.swift
@@ -40,20 +40,21 @@ public struct XLPagerView<Content> : View where Content : View {
     private var type: PagerType
     private var content: () -> Content
     
-    @State private var indexSelected: Int
+    @State private var currentPage: Int
+    @State private var currentOffset: CGFloat = 0
     
     public init(_ type: PagerType = .twitter,
                 selection: Int = 1,
                 @ViewBuilder content: @escaping () -> Content) {
         self.type = type
         self.content = content
-        self._indexSelected = State(initialValue: selection)
+        self._currentPage = State(initialValue: selection)
     }
     
     public var body: some View {
         VStack {
             if type == .youtube {
-                NavBar(selection: self.$indexSelected)
+                NavBar(selection: self.$currentPage)
                     .frame(width: 100, height: 40, alignment: .center)
                     .background(Color.red)
             }
@@ -66,9 +67,9 @@ public struct XLPagerView<Content> : View where Content : View {
                         }
                         .background(Color.blue)
                     }
-                    .onChange(of: self.indexSelected) { index in
+                    .onChange(of: self.currentPage) { index in
                         withAnimation {
-                            sproxy.scrollTo(indexSelected)
+                            sproxy.scrollTo(currentPage)
                         }
                     }
                 }
