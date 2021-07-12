b37384064ca72c324a41096635f080d28c3bcc37
cecipirotto <31603856+cecipirotto@users.noreply.github.com>
2021-07-12 16:09:29 -0300

    Indicator bar, onAppear and highlighted (#11)
    * on highlighted status
    
    * Add onPageAppear callback
    
    * indicator bar and fix selected
    
    * call onPagerAppear callback and fixes
    
    * change request

--- Sources/XLPagerView.swift
+++ Sources/XLPagerView.swift
@@ -11,6 +11,10 @@ extension View {
     public func pagerTabItem<V>(@ViewBuilder _ pagerTabView: @escaping () -> V) -> some View where V: View {
         return self.modifier(PagerTabItem(navTabView: pagerTabView))
     }
+
+    public func onPageAppear(perform action: (() -> Void)? = nil) -> some View {
+        return self.modifier(PagerSetAppearItem(onPageAppear: action ?? {}))
+    }
 }
 
 struct PagerTabView<Content: View, NavTabView: View>: View {
@@ -26,6 +30,34 @@ struct PagerTabView<Content: View, NavTabView: View>: View {
     
     var body: some View {
         content()
+    }
+}
+
+struct PagerSetAppearItem: ViewModifier {
+    @EnvironmentObject var navContentViews : DataStore
+    @EnvironmentObject var pagerSettings: PagerSettings
+    var onPageAppear: () -> Void
+    @State var index = -1
+
+    init(onPageAppear: @escaping () -> Void) {
+        self.onPageAppear = onPageAppear
+    }
+
+    func body(content: Content) -> some View {
+        content
+            .frame(width: pagerSettings.width, height: pagerSettings.height)
+            .overlay(
+                GeometryReader { reader in
+                    Color.clear
+                        .onAppear {
+                            DispatchQueue.main.async {
+                                let frame = reader.frame(in: .named("XLPagerViewScrollView"))
+                                index = Int(round((frame.minX - pagerSettings.contentOffset) / pagerSettings.width))
+                                navContentViews.setAppear(callback: onPageAppear, at: index)
+                            }
+                        }
+                }
+            )
     }
 }
 
@@ -72,6 +104,10 @@ struct NavBarModifier: ViewModifier {
     @EnvironmentObject var pagerSettings: PagerSettings
     @Binding private var indexSelected: Int
     @Binding private var itemCount: Int
+    private var navBarItemWidth: CGFloat {
+        let totalItemWidth = (pagerSettings.width - (pagerSettings.tabItemSpacing * CGFloat(itemCount - 1)))
+        return totalItemWidth / CGFloat(itemCount)
+    }
     
     public init(itemCount: Binding<Int>, selection: Binding<Int>) {
         self._indexSelected = selection
@@ -79,11 +115,9 @@ struct NavBarModifier: ViewModifier {
     }
     
     func body(content: Content) -> some View {
-        VStack(alignment: .leading) {
+        VStack(alignment: .leading, spacing: 0) {
             LazyHStack(spacing: pagerSettings.tabItemSpacing) {
                 if itemCount > 0 && pagerSettings.width > 0 {
-                    let totalItemWidth = (pagerSettings.width - (pagerSettings.tabItemSpacing * CGFloat(itemCount - 1)))
-                    let navBarItemWidth: CGFloat = totalItemWidth / CGFloat(itemCount)
                     ForEach(0...itemCount-1, id: \.self) { idx in
                         NavBarItem(id: idx, selection: $indexSelected)
                             .frame(width: navBarItemWidth, height: pagerSettings.tabItemHeight, alignment: .center)
@@ -91,6 +125,16 @@ struct NavBarModifier: ViewModifier {
                 }
             }
             .frame(width: pagerSettings.width, height: pagerSettings.tabItemHeight, alignment: .center)
+            HStack {
+                if let width = navBarItemWidth, width > 0, width <= pagerSettings.width {
+                    let x = -self.pagerSettings.contentOffset / CGFloat(itemCount) + width / 2
+                    Rectangle()
+                        .fill(pagerSettings.indicatorBarColor)
+                        .frame(width: width)
+                        .position(x: x, y: 0)
+                }
+            }
+            .frame(width: pagerSettings.width, height: pagerSettings.indicatorBarHeight)
             content
         }
     }
@@ -133,7 +177,10 @@ struct NavBarItem: View {
                 self.indexSelected = id
             }, label: {
                 navContentViews.items.value[id]?.view
-            })
+            }).buttonStyle(PlainButtonStyle())
+            .onLongPressGesture(minimumDuration: 0, maximumDistance: .infinity) { pressing in
+                navContentViews.items.value[id]?.tabViewDelegate?.setSelectedState(state: pressing ? .highlighted : .selected)
+            } perform: {}
         }
     }
 }
@@ -144,17 +191,19 @@ public enum PagerType {
 }
 
 public class PagerSettings: ObservableObject {
-    @Published var width: CGFloat
-    @Published var height: CGFloat
+    @Published var width: CGFloat = 0
+    @Published var height: CGFloat = 0
     @Published var tabItemSpacing: CGFloat
     @Published var tabItemHeight: CGFloat
+    @Published var indicatorBarHeight: CGFloat
+    @Published var indicatorBarColor: Color
     @Published var contentOffset: CGFloat = 0
     
-    public init(width: CGFloat = 0, height: CGFloat = 0, tabItemSpacing: CGFloat = 5, tabItemHeight: CGFloat = 100) {
-        self.width = width
-        self.height = height
+    public init(tabItemSpacing: CGFloat = 5, tabItemHeight: CGFloat = 100, indicatorBarHeight: CGFloat = 1.5, indicatorBarColor: Color = .blue) {
         self.tabItemSpacing = tabItemSpacing
         self.tabItemHeight = tabItemHeight
+        self.indicatorBarHeight = indicatorBarHeight
+        self.indicatorBarColor = indicatorBarColor
     }
 }
 
@@ -176,7 +225,7 @@ public struct XLPagerView<Content> : View where Content : View {
     
     @State private var itemCount : Int = 0
     @GestureState private var translation: CGFloat = 0
-    
+
     public init(_ type: PagerType = .twitter,
                 selection: Int = 0,
                 pagerSettings: PagerSettings,
@@ -219,30 +268,32 @@ public struct XLPagerView<Content> : View where Content : View {
                         }
                     }
                 )
+                .clipped()
                 .onChange(of: self.currentIndex) { [currentIndex] newIndex in
                     self.currentOffset = self.offsetForPageIndex(newIndex)
+                    if let callback = navContentViews.items.value[currentIndex]?.appearCallback {
+                        callback()
+                    }
                     if let tabViewDelegate = navContentViews.items.value[currentIndex]?.tabViewDelegate {
                         tabViewDelegate.setSelectedState(state: .normal)
                     }
-                    if let tabViewDelegate = navContentViews.items.value[newIndex]?.tabViewDelegate {
+                    if let tabViewDelegate = navContentViews.items.value[newIndex]?.tabViewDelegate, newIndex != currentIndex {
                         tabViewDelegate.setSelectedState(state: .selected)
                     }
                 }
-                .onChange(of: self.pagerSettings.width) { _ in
+                .onChange(of: translation) { _ in
+                    self.pagerSettings.contentOffset = translation - CGFloat(currentIndex)*pagerSettings.width
+                }
+                .onChange(of: pagerSettings.width) { _ in
                     self.currentOffset = self.offsetForPageIndex(self.currentIndex)
                 }
                 .onChange(of: itemCount) { _ in
                     currentIndex = currentIndex >= itemCount ? itemCount - 1 : currentIndex
                 }
                 .onAppear {
-                    if pagerSettings.width == 0 {
-                        pagerSettings.width = gproxy.size.width
-                    }
-                    if pagerSettings.height == 0 {
-                        pagerSettings.height = gproxy.size.height
-                    }
+                    pagerSettings.width = gproxy.size.width
+                    pagerSettings.height = gproxy.size.height
                 }
-                .clipped()
             }
         }
         .navBar(itemCount: $itemCount, selection: $currentIndex)
@@ -259,7 +310,7 @@ public struct XLPagerView<Content> : View where Content : View {
 
 public enum PagerTabViewState {
     case selected
-    case hightlighted
+    case highlighted
     case normal
 }
 
