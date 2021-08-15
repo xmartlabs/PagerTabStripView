e547f80a1acf7b6662402a700687664b4cac87a8
Martin Barreto <martin@xmartlabs.com>
2021-08-14 21:53:15 -0300

    clean up code

--- Sources/PagerSetAppearModifier.swift
+++ Sources/PagerSetAppearModifier.swift
@@ -16,19 +16,16 @@ struct PagerSetAppearItemModifier: ViewModifier {
     }
 
     func body(content: Content) -> some View {
-        content
-            .overlay(
-                GeometryReader { reader in
-                    Color.clear
-                        .onAppear {
-                            DispatchQueue.main.async {
-                                let frame = reader.frame(in: .named("PagerViewScrollView"))
-                                index = Int(round(frame.minX / settings.width))
-                                dataStore.setAppear(callback: onPageAppear, at: index)
-                            }
-                        }
+        GeometryReader { reader in
+            content
+                .onAppear {
+                    DispatchQueue.main.async {
+                        let frame = reader.frame(in: .named("PagerViewScrollView"))
+                        index = Int(round(frame.minX / settings.width))
+                        dataStore.setAppear(callback: onPageAppear, at: index)
+                    }
                 }
-            )
+            }
     }
     
     @EnvironmentObject private var dataStore: DataStore
--- Sources/PagerTabItemModifier.swift
+++ Sources/PagerTabItemModifier.swift
@@ -16,26 +16,23 @@ struct PagerTabItemModifier<NavTabView: View> : ViewModifier {
     }
 
     func body(content: Content) -> some View {
-        content
-            .overlay(
-                GeometryReader { reader in
-                    Color.clear
-                        .onAppear {
-                            DispatchQueue.main.async {
-                                let frame = reader.frame(in: .named("PagerViewScrollView"))
-                                index = Int(round(frame.minX / settings.width))
-                                let tabView = navTabView()
-                                let tabViewDelegate = tabView as? PagerTabViewDelegate
-                                dataStore.setView(AnyView(tabView),
-                                                        tabViewDelegate: tabViewDelegate,
-                                                        at: index)
-                            }
-                        }.onDisappear {
-                            dataStore.items[index]?.tabViewDelegate?.setState(state: .normal)
-                            dataStore.remove(at: index)
-                        }
+        GeometryReader { reader in
+            content
+                .onAppear {
+                    DispatchQueue.main.async {
+                        let frame = reader.frame(in: .named("PagerViewScrollView"))
+                        index = Int(round(frame.minX / settings.width))
+                        let tabView = navTabView()
+                        let tabViewDelegate = tabView as? PagerTabViewDelegate
+                        dataStore.setView(AnyView(tabView),
+                                                tabViewDelegate: tabViewDelegate,
+                                                at: index)
+                    }
+                }.onDisappear {
+                    dataStore.items[index]?.tabViewDelegate?.setState(state: .normal)
+                    dataStore.remove(at: index)
                 }
-            )
+        }
     }
     
     @EnvironmentObject private var dataStore: DataStore
