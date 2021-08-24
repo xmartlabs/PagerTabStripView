3817da1d5b0c15c5995b0081b70f4e3542d7ff63
Martin Barreto <martin@xmartlabs.com>
2021-08-24 17:15:26 -0300

    Create new target - its a macOS framework

---
+++ PagerTabStripView-MacOSTests/PagerTabStripView_MacOSTests.swift
@@ -0,0 +1,33 @@
+//
+//  PagerTabStripView_MacOSTests.swift
+//  PagerTabStripView-MacOSTests
+//
+//  Created by Martin Barreto on 24/8/21.
+//
+
+import XCTest
+@testable import PagerTabStripView_MacOS
+
+class PagerTabStripView_MacOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
