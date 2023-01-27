ff3ad6ded521849b908bad8987b1676461a8d213
Martin Barreto <mtn.barreto@gmail.com>
2023-01-27 00:32:25 -0300

    delete mac os testing files

--- Example/Tests iOS/Tests_iOS.swift
+++ Example/Tests iOS/Tests_iOS.swift
@@ -33,7 +33,7 @@ class Tests_iOS: XCTestCase {
     }
 
     func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+        if #available(iOS 13.0, *) {
             // This measures how long it takes to launch your application.
             measure(metrics: [XCTApplicationLaunchMetric()]) {
                 XCUIApplication().launch()
